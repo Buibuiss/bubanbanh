81a5b91431a35fbfc6d2671a7a550d5f41b3736d
Buibuiss <nguyenhuynhat2048@gmail.com>
2024-06-26 23:41:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -10,29 +10,6 @@ import Search from './pages/Search';
 //component
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: 'AIzaSyAWLC8DDKKD-A-18JRYn8bqOoKNOMTwAfM',
-    authDomain: 'bubu-cake.firebaseapp.com',
-    projectId: 'bubu-cake',
-    storageBucket: 'bubu-cake.appspot.com',
-    messagingSenderId: '366887621579',
-    appId: '1:366887621579:web:6616f0096ace1273531c4e',
-    measurementId: 'G-WDS0DJWLNN',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 //layout
 const Layout = () => {
     return (
